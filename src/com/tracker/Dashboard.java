ac454954352ad80a0da2ce0071400c26b0baa61c
Karthikeyan K <karthikeyankdeveloper@gmail.com>
2023-12-25 18:58:18 +0530

    Initial commit

---
+++ src/com/tracker/Dashboard.java
@@ -0,0 +1,14 @@
+package com.tracker;
+
+public class Dashboard {
+	static {
+		System.out.println("Welcome to Live Location Tracker");
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
